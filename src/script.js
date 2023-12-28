89d20b6c1cbfb81467aa37cd419c57acaff59d37
Aydar N. (Sublime Merge MBP) <me@aydar.media>
2023-12-28 13:38:21 +0300

    added window img

--- src/script.js
+++ src/script.js
@@ -1,1 +1,26 @@
 import './index.scss'
+
+import gsap from 'gsap'
+
+const windowImg = document.getElementById('image-swap');
+
+if (windowImg) {
+  const srcs = windowImg ? windowImg.getAttribute('data-images').split(',') : null;
+
+  function changeSrc() {
+    const index = Math.floor(gsap.getProperty(this, 'progress') * srcs.length);
+    if (index < srcs.length) {
+      windowImg.src = srcs[index];
+    }
+  }
+
+  const animate = () => {
+    gsap.to({}, {
+      duration: 2,
+      onUpdate: changeSrc,
+      onComplete: animate
+    });
+  }
+
+  animate()
+}
